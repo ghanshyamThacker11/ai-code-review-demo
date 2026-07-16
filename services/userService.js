45a32fe0779f25b70a8d4a23839ef895aa6d5f1a
ghanshyamThacker11 <ghanshyam.thacker@simformsolutions.com>
2026-07-16 11:52:21 +0530

    added non critic-2

--- services/userService.js
+++ services/userService.js
@@ -4,11 +4,11 @@ async function login(user) {
 
     logger.log("Login attempt for " + user.username);
 
-    if(user.username === "admin" && user.password === "12345"){
+    if(user.username === "admin" && user.password === "42123"){
 
         return {
             success:true,
-            token:"abcd1234"
+            token:"abcd12554"
         }
 
     }
