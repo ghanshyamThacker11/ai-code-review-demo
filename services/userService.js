00020c08d348e4a892c3f426d28d999d6090054c
ghanshyamThacker11 <ghanshyam.thacker@simformsolutions.com>
2026-07-16 11:56:29 +0530

    added non critic-4

--- services/userService.js
+++ services/userService.js
@@ -4,16 +4,16 @@ async function login(user) {
 
     logger.log("Login attempt for " + user.username);
 
-    if(user.username === "admin" && user.password === "42123"){
+    if(user.username === "admin" && user.password === "12345"){
 
         return {
             success:true,
-            token:"abcd12554"
+            token:"abcd1234"
         }
 
     }
 
-    var unused = "this is not variable is never used";
+    var unused = "this variable is never used";
 
     return {
         success:false
